9ecaf55be603a6220394569680217642a6395b4a
jozoshu <mj93m@naver.com>
2021-09-07 02:36:51 +0900

    feat: logger 세팅

---
+++ modules/logging/decorators.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+from modules.logging import get_logger
+from .utils import env_info
+
+
+def manager_logging(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger = get_logger('main')
+        env_info(logger)
+
+        logger.info('RUNNING MANAGER')
+        logger.info('='*80)
+        func(*args, **kwargs)
+        logger.info('='*80)
+        logger.info('FINISHED')
+
+    return wrapper
---
+++ modules/logging/utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+
+def env_info(logger):
+    """
+    :Description
+        실행한 환경 출력
+
+    :Info
+        Root Dir: 실행되는 위치
+        Python: python 버전
+        DB Host: 연결한 데이터베이스 host
+        DB Name: 연결한 데이터베이스명
+    """
+    logger.info(f'## - ENV INFO - ##')
+    root_dir = os.environ.get('PWD')
+    logger.info(f'Root Dir: {root_dir}')
+    version = ".".join(map(str, sys.version_info[:3]))
+    logger.info(f'Python: {version}')
+    db_host = os.environ.get('DB_HOST')
+    logger.info(f'DB Host: {db_host}')
+    db_name = os.environ.get('DB_NAME')
+    logger.info(f'DB NAME: {db_name}')
