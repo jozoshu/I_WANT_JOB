36bb61a0bde8303b1248f0b4fcd6c2a91ec5c13c
jozoshu <mj93m@naver.com>
2021-09-06 22:45:49 +0900

    feat: 크롤러 작성

---
+++ modules/handlers/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+from modules.db.connections import DBConnection as db
+
+
+class BaseHandler(ABC):
+    """베이스 handler"""
+
+    def __init__(self):
+        self._conn = db.get_conn()
+
+    @property
+    def conn(self):
+        return self._conn
+
+    @abstractmethod
+    def handle(self, *args, **kwargs):
+        raise NotImplementedError('You Must Implement `handle` Method.')
