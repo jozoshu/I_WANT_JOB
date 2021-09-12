f622bd5675b71271c47a36a73a757489dc70792f
jozoshu <mj93m@naver.com>
2021-09-13 00:21:23 +0900

    fix: timezone KST 설정

--- modules/handlers/base.py
+++ modules/handlers/base.py
@@ -14,7 +14,7 @@ class BaseHandler(ABC):
 
     def __init__(self):
         self._conn = db.get_conn()
-        self.crawl_date = KST.localize(datetime.now()).date()
+        self.crawl_date = datetime.now(KST).date()
 
     @property
     def conn(self):
