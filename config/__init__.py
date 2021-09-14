a26b114449c2ea4a983276971ad95fe887cfd417
jozoshu <mj93m@naver.com>
2021-09-14 16:47:35 +0900

    feat: 작업디렉토리 고정

--- config/__init__.py
+++ config/__init__.py
@@ -4,10 +4,17 @@ env = os.environ.get('CRAWLER_DEFAULT_ENV')
 
 
 class Settings:
-    _lib = __import__(f'config.settings.{env}', fromlist=['*'])
+    def __init__(self):
+        _lib = __import__(f'config.settings.{env}', fromlist=['*'])
 
-    for k in dir(_lib):
-        locals()[k] = getattr(_lib, k)
+        for key in dir(_lib):
+            value = getattr(_lib, key)
+            setattr(self, key, value)
+
+    def __setattr__(self, key, value):
+        if key == 'BASE_DIR':
+            os.chdir(value)
+        super().__setattr__(key, value)
 
 
 settings = Settings()
